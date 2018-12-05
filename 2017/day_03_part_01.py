a42e08628c7292fab06fd3f41685d18cdce4665e
John Kitto <johnakitto@gmail.com>
2018-12-05 00:42:07 -0500

    removed old day 3 files

--- 2017/day_03_part_01.py
+++
@@ -1,8 +0,0 @@
-from math import sqrt, ceil
-
-n = int(input('enter a number in the spiral: '))
-
-layer = lambda n: ceil((sqrt(n)-1) / 2)
-deviation_from_axis = lambda n, layer: abs((layer-1) - ((n-2) % (2*layer)))
-
-print('steps from center: '+ str(layer(n) + deviation_from_axis(n, layer(n))))
