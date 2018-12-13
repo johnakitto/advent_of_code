8aaef25794715e2331281608065db14318282ddd
John Kitto <johnakitto@gmail.com>
2018-12-13 09:32:58 -0500

    day 4

--- 2018/day_04.py
+++ 2018/day_04.py
@@ -3,11 +3,9 @@ start_time = time.time()
 
 records = [record.strip('\n') for record in open('day_04.txt', 'r').readlines()]
 
-guard_ids, dates = [], []
-dates_to_times, dates_to_guards = {}, {}
+dates_to_times, dates_to_guards, guard_sleep_counts = {}, {}, {}
 for record in records:
 	if '#' in record:
-		guard_id = record.split('#')[1].split()[0]
 		start_hour = int(record.split(':')[0][-2:])
 		start_minute = int(record.split(':')[1][:2])
 		shift_date = record.split()[0][1:].split('-')
@@ -15,11 +13,10 @@ for record in records:
 		if start_hour==23:
 			shift_date += dt.timedelta(days=1)
 			start_minute = 0
-		guard_ids.append(guard_id)
 		dates_to_times[str(shift_date)] = [start_minute]
+		guard_id = record.split('#')[1].split()[0]
 		dates_to_guards[str(shift_date)] = guard_id
-
-guard_sleep_counts = {guard_id: [0]*60 for guard_id in guard_ids}
+		guard_sleep_counts[guard_id] = [0]*60
 
 for record in records:
 	if '#' not in record:
@@ -27,8 +24,8 @@ for record in records:
 		wake_or_sleep_minute = int(record.split(':')[1][:2])
 		dates_to_times[shift_date].append(wake_or_sleep_minute)
 
-for date in dates_to_times.keys():
-	times = sorted(dates_to_times[date])
+for date, times in dates_to_times.items():
+	times = sorted(times)
 	if len(times) > 1:
 		minutes_slept = [minute for i in range(1, len(times)-1, 2) for minute in range(times[i], times[i+1])]
 		for minute in minutes_slept:
@@ -41,6 +38,7 @@ print('|  '*61)
 
 sleepiest_guard = most_total_sleep = sleepiest_guards_sleepiest_minute = 0
 consistent_sleeper = most_sleepy_minute = consistent_sleepers_sleepiest_minute = 0
+
 guard_sleep_counts = {int(guard_id): counts for guard_id, counts in guard_sleep_counts.items()}
 for guard, sleep_counts in collections.OrderedDict(sorted(guard_sleep_counts.items())).items():
 	total_sleep = sum(sleep_counts)
